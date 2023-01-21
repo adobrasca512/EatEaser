d8b20e31ad2e64ddd82f0720f78936d57401e175
Adilem Dobras Castillo <59998402+adobrasca512@users.noreply.github.com>
2023-01-21 13:59:25 +0100

    x

--- Interfaz/main.py
+++ Interfaz/main.py
@@ -1013,6 +1013,8 @@ class Train(Index):
         
         self.switchButtons()
     def setWidgets(self):
+        #
+        self.matrizlt=self.findChild(QVBoxLayout,'verticalLayout_3')
         
         self.seleccionados = []
         self.checkboxes = []
